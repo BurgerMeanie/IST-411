acb0b5e21442eeb634049b454ed94b959ffeda1f
hxb5237 <hxb5237@psu.edu>
2023-01-24 14:53:09 +0900

    add bank deadlock and bank classes

---
+++ Deadlock/src/Bank.java
@@ -0,0 +1,65 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+import java.util.Arrays;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+/*
+Project: Lab 2
+Purpose Details: Threading Synchronization
+Course: IST 411
+Author: Team 1 (Kelly Bergamini, William Allwein, Hun Bae, Phillip Berry, Allea McFarlane)
+Date Developed: 1/24/2023
+Last Date Changed:
+Revision: 1
+*/
+
+public class Bank {
+    private final double[] accounts;
+    private Lock bankLock = new ReentrantLock();
+    private Condition sufficientFunds = bankLock.newCondition();
+
+    public Bank(int n, double initialBalance){
+        accounts = new double[n];
+        Arrays.fill(accounts, initialBalance);
+    }
+
+    public void transfer(int from, int to, double amount) throws InterruptedException{
+        bankLock.lock();
+        try{
+            while(accounts[from] < amount){
+                sufficientFunds.wait();
+            }
+            System.out.print(Thread.currentThread());
+            accounts[from] -= amount;
+            System.out.printf(" %10.2f from %d to %d", amount, from, to);
+            accounts[to] += amount;
+            System.out.printf(" Total Balance: %10.2f%n", getTotalBalance());
+            sufficientFunds.signal();
+        }finally{
+            bankLock.unlock();
+        }
+    }
+
+    public double getTotalBalance(){
+        bankLock.lock();
+        try{
+            double sum = 0;
+            for(double a : accounts){
+                sum+= a;
+            }
+            return sum;
+        }finally{
+            bankLock.unlock();
+        }
+    }
+
+    public int size(){
+        return accounts.length;
+    }
+
+}
--- Deadlock/src/BankDeadlock.java
+++ Deadlock/src/BankDeadlock.java
@@ -3,8 +3,6 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 
-package deadlock;
-
 /*
 Project: Lab 2
 Purpose Details: Threading Synchronization
@@ -15,7 +13,7 @@ Last Date Changed:
 Revision: 1
 */
 
-public class Deadlock {
+public class BankDeadlock {
 
     public static final int NACCOUNTS = 100;
     public static final double INITIAL_BALANCE = 1000;
@@ -23,7 +21,23 @@ public class Deadlock {
     public static final int DELAY = 1000;
 
     public static void main(String[] args) {
-        Bank bank = new Bank()
+        Bank bank = new Bank(NACCOUNTS, INITIAL_BALANCE);
+        for (int i = 0; i < NACCOUNTS; i++){
+            int fromAccount = i;
+            Runnable r = () -> {
+                try{
+                    while (true){
+                        int toAccount = (int) (bank.size() * Math.random());
+                        double amount = MAX_AMOUNT * Math.random();
+                        bank.transfer(toAccount, fromAccount, amount);
+                    }
+                } catch (InterruptedException ex){
+
+                }
+            };
+            Thread t = new Thread(r);
+            t.start();
+        }
     }
 
 }
