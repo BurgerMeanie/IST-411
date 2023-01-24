3ed35a2a9d60ece8ad29de160a5bf1522efad746
hxb5237 <hxb5237@psu.edu>
2023-01-24 14:25:31 +0900

    add deadlock project

---
+++ Deadlock/src/deadlock/BankDeadlock.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+package deadlock;
+
+/*
+Project: Lab 2
+Purpose Details: Threading Synchronization
+Course: IST 411
+Author: Team 1 (Kelly Bergamini, William Allwein, Hun Bae, Phillip Berry, Allea McFarlane)
+Date Developed: 1/24/2023
+Last Date Changed:
+Revision: 1
+*/
+
+public class Deadlock {
+
+    public static final int NACCOUNTS = 100;
+    public static final double INITIAL_BALANCE = 1000;
+    public static final double MAX_AMOUNT = 1000;
+    public static final int DELAY = 1000;
+
+    public static void main(String[] args) {
+        Bank bank = new Bank()
+    }
+
+}
